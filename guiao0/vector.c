cde7247a16b5b691ae9eee816a492c8639b5f80b
tgbz <tgomesbz@gmail.com>
2020-02-29 18:43:20 +0000

    pausa

---
+++ guiao0/vector.c
@@ -0,0 +1,18 @@
+
+
+void fill(int* vector, int size, int value){
+    for (int i=0; i<size;i++){
+        *(vector + i) = value;
+    }
+}
+
+int find(int* vector, int size, int value){
+    int n = 0;
+    for (int i = 0; i < size; ++i){
+        if(*(vector + 1) == value){
+            n++;
+        }
+    }
+    return n;
+}
+
